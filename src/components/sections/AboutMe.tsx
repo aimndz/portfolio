94548d7e1256cd19d4d2a212ca59d505d0e57d88
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2025-02-08 01:53:10 +0800

    Feat: Add about me content

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -4,14 +4,29 @@ function AboutMe() {
   return (
     <div className="max-w-4xl mx-10 mt-52">
       <SectionTitle title="About me" />
-      <p className="text-p-muted md:text-lg text-md">
-        I am a full-stack developer with a passion for both coding and
-        creativity. I specialize in crafting pixel-perfect user interfaces that
-        seamlessly combine intuitive design with solid engineering. My best work
-        emerges at the crossroads of design and development, where I create
-        experiences that are not only visually striking but also optimized for
-        performance and usability.
-      </p>
+      <div className="text-p-muted md:text-lg text-md">
+        <p>
+          I&apos;m a{" "}
+          <span className="text-p-default">full-stack developer</span> and an{" "}
+          <span className="text-p-default">artist</span> who sees programming
+          the same way I see art. I was an artist first before diving into
+          development, so that creative mindset naturally carried over. I also
+          love <span className="text-p-default">solving problems</span> (which I
+          sometimes create myself), which makes coding the perfect blend of
+          logic and creativity for me.
+        </p>
+        <br />
+        <p>
+          In my spare time, you&apos;ll probably find me learning something new
+          on tech (lol nerd). Other than that, you&apos;ll find me playing
+          basketball, drawing, or playing video games. Whether it&apos;s art,
+          code, or strategy, I believe in creating, innovating, and having fun
+          along the way.{" "}
+          <span className="text-p-default">
+            Let&apos;s build something awesome together!
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
