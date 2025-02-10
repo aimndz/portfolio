88e18c96b878282d3d76227db76d97d542d7cc35
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2025-02-11 05:44:30 +0800

    Feat: Add animation on technologies

--- src/components/sections/Technologies.tsx
+++ src/components/sections/Technologies.tsx
@@ -1,5 +1,6 @@
 import { content } from "@/lib/constants";
 import SectionTitle from "../SectionTitle";
+import { motion } from "motion/react";
 
 function Technologies() {
   const { technologies } = content;
@@ -10,14 +11,18 @@ function Technologies() {
       <div className="grid grid-cols-1 min-[500px]:grid-cols-2 md:grid-cols-4 gap-3 ">
         {technologies.map((tech, index) => {
           return (
-            <div
+            <motion.div
               key={index}
               className="flex items-center space-x-4 bg-s-muted md:p-4 p-2 rounded-lg border border-s-default relative overflow-hidden"
+              initial={{ opacity: 0, y: 100, filter: "blur(2px)" }}
+              whileInView={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+              transition={{ duration: 0.5 }}
+              viewport={{ amount: 0.1 }}
             >
               <i>{tech.icon}</i>
               <p className="text-p-default">{tech.name}</p>
               <div className="absolute top-10 left-1/2 transform -translate-x-1/2 w-[150%] aspect-square rounded-full blur-lg bg-white opacity-10"></div>
-            </div>
+            </motion.div>
           );
         })}
       </div>
