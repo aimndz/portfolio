f116b535cb1e3e9b182424455c712844ec03b2dd
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2025-02-11 05:58:21 +0800

    UI: Change footer styles

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,7 +1,14 @@
 function Footer() {
   return (
     <footer className="text-center mb-10 text-sm mx-5">
-      <p>© 2025 Amiel Ian Mendoza. All rights reserved.</p>
+      <div className="flex flex-col md:flex-row md:justify-center md:space-x-2 text-lg">
+        <p>
+          <span className="block md:inline-block">
+            © 2025 Amiel Ian Mendoza.
+          </span>{" "}
+          <span className="block md:inline-block">All rights reserved.</span>
+        </p>
+      </div>
       <p className="text-p-muted">
         Built with love, patience, and questionable decisions.
       </p>
