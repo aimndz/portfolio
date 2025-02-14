70d7132d5d0040139fe31ea1877478197424c5e4
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2025-02-15 03:45:48 +0800

    Feat: Set up about-me page

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
 import SectionTitle from "../SectionTitle";
 import { motion } from "framer-motion";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
 
 function AboutMe() {
   return (
@@ -40,6 +43,24 @@ function AboutMe() {
           </span>
         </motion.p>
       </div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        viewport={{ amount: 0.1 }}
+        className="group mt-10 flex justify-center md:justify-end"
+      >
+        <Link
+          href="/about-me"
+          className="relative inline-block overflow-hidden rounded-full"
+        >
+          <div className="glow-top"></div>
+          <Button className="text-md hover:text-accent-30 rounded-full border border-s-default bg-s-muted px-6 py-6 font-medium uppercase text-accent-100 transition-all duration-300 hover:bg-s-muted">
+            <span>Let&apos;s get personal</span> <ArrowRight />
+          </Button>
+          <div className="absolute left-1/2 top-10 aspect-square w-[150%] -translate-x-1/2 transform rounded-full bg-[#26B0CE] opacity-0 blur-lg group-hover:opacity-30"></div>
+        </Link>
+      </motion.div>
     </div>
   );
 }
