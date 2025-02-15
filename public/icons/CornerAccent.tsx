b49e2f0f6cb3cce6fba4ec7b340cd7f1c533663e
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2025-02-16 02:39:43 +0800

    Chore: Remove unnecessary motion.div on section title

--- public/icons/CornerAccent.tsx
+++ public/icons/CornerAccent.tsx
@@ -10,7 +10,7 @@ function CornerAccent({ className }: { className?: string }) {
     >
       <path
         d="M1 29V9.90909C1 8.35354 1.52258 4.81818 3.6129 3.12121C5.70323 1.42424 8.54839 1 9.70968 1H28"
-        stroke="white"
+        stroke="#0C3741"
       />
     </svg>
   );
--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -3,18 +3,19 @@ import { motion } from "motion/react";
 
 function SectionTitle({ title }: { title: string }) {
   return (
-    <motion.h2
-      className="glowing-gradient-text relative mb-10 inline-block p-4 font-montserrat text-xl font-extrabold md:mb-20 md:p-8 md:text-3xl"
-      initial={{ opacity: 0, y: 20 }}
-      whileInView={{ opacity: 1, y: 0 }}
+    <motion.div
+      initial={{ opacity: 0, x: -30 }}
+      whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <CornerAccent className="absolute left-0 top-0" />
-      <motion.p>{title}</motion.p>
-      <motion.div layout>
-        <CornerAccent className="absolute bottom-0 right-0 rotate-180" />
-      </motion.div>
-    </motion.h2>
+      <h2 className="glowing-gradient-text relative z-50 mb-10 inline-block p-4 font-montserrat text-xl font-extrabold md:mb-10 md:p-8 md:text-3xl">
+        <CornerAccent className="absolute left-0 top-0" />
+        <p>{title}</p>
+        <div>
+          <CornerAccent className="absolute bottom-0 right-0 rotate-180" />
+        </div>
+      </h2>
+    </motion.div>
   );
 }
 
