9ed44a24193cd6ba8b9b51bd13ce2e38da8030e9
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2025-09-18 22:38:34 +0800

    fix: social ui link not working on mobile view

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -21,7 +21,12 @@ function AboutMe() {
           <Separator className="my-3 bg-[#0C3741] md:my-4" />
           <div className="flex justify-center gap-3 md:flex-col md:gap-0">
             <div className="flex items-center justify-center gap-3">
-              <Mail size={15} />
+              <Link
+                href="mailto:amiel.ian.mendoza@gmail.com"
+                className="-m-2 rounded-md p-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent-100 md:-m-0 md:p-0"
+              >
+                <Mail size={20} />
+              </Link>
               <Link
                 href="mailto:amiel.ian.mendoza@gmail.com"
                 className="hidden hover:underline md:block"
@@ -30,24 +35,32 @@ function AboutMe() {
               </Link>
             </div>
             <div className="flex items-center gap-3">
-              <svg
-                width={15}
-                height={15}
-                className="shrink-0"
-                xmlns="http://www.w3.org/2000/svg"
-                shapeRendering="geometricPrecision"
-                textRendering="geometricPrecision"
-                imageRendering="optimizeQuality"
-                fillRule="evenodd"
-                clipRule="evenodd"
-                viewBox="0 0 512 462.799"
+              <Link
+                href="https://x.com/aim_mndz"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Open X (Twitter) profile"
+                className="-m-2 rounded-md p-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent-100 md:-m-0 md:p-0"
               >
-                <path
-                  fill="#fff"
-                  fillRule="nonzero"
-                  d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"
-                />
-              </svg>
+                <svg
+                  width={18}
+                  height={18}
+                  className="shrink-0"
+                  xmlns="http://www.w3.org/2000/svg"
+                  shapeRendering="geometricPrecision"
+                  textRendering="geometricPrecision"
+                  imageRendering="optimizeQuality"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  viewBox="0 0 512 462.799"
+                >
+                  <path
+                    fill="#fff"
+                    fillRule="nonzero"
+                    d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"
+                  />
+                </svg>
+              </Link>
               <Link
                 href="https://x.com/aim_mndz"
                 target="_blank"
