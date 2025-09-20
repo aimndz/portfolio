257cff8992eb4a5e16859550606fbcac042af1bb
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2025-09-20 22:46:56 +0800

    feat: add about me placeholder

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,5 +1,15 @@
 function AboutMe() {
-  return <div>about-me</div>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="mx-3 flex flex-col items-center justify-center">
+        <p className="mb-3 text-4xl font-bold">Oops!</p>
+        <div className="text-center font-dm_mono italic text-white/60">
+          <p className="">I&apos;m not quite ready to get personal. </p>
+          <p> Check back soon!</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
