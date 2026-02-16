3f86aa78da3667b5c4399e7b7f4e288887a985dc
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2026-02-17 03:19:03 +0800

    fix: hydration issue

--- src/components/NavigationTab.tsx
+++ src/components/NavigationTab.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useRouter, usePathname } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 import dynamic from "next/dynamic";
@@ -39,24 +40,37 @@ function NavigationTab() {
 
           <TabsList className="fixed top-10 left-1/2 z-999 h-12 w-52 -translate-x-1/2 transform border border-[#0c374180] bg-[#06232b80] backdrop-blur-md">
             <div className="glow-top"></div>
-            <TabsTrigger value="home" className="h-9 w-full uppercase">
+            <TabsTrigger
+              value="home"
+              id="tabs-trigger-home"
+              aria-label="Home"
+              className="h-9 w-full uppercase"
+            >
               Home
             </TabsTrigger>
-            <TabsTrigger value="projects" className="h-9 w-full uppercase">
+            <TabsTrigger
+              value="projects"
+              id="tabs-trigger-projects"
+              aria-label="Projects"
+              className="h-9 w-full uppercase"
+            >
               Proj
             </TabsTrigger>
           </TabsList>
 
-          <Link href={content.socialLinks.resume} target="_blank">
-            <Button className="text-md text-p-default hover:bg-s-muted hover:text-accent-100 fixed right-6 z-999 hidden gap-1 rounded-full bg-transparent px-6 py-6 font-medium uppercase transition-all duration-300 min-[900px]:right-24 md:top-10 md:flex">
-              <span>Resume</span> <ArrowUpRight />
-            </Button>
+          <Link
+            href={content.socialLinks.resume}
+            target="_blank"
+            aria-label="View Resume"
+            className="text-md text-p-default hover:bg-s-muted hover:text-accent-100 fixed right-6 z-999 hidden items-center justify-center gap-1 rounded-full border border-transparent bg-transparent px-6 py-3 font-medium uppercase shadow-sm transition-all duration-300 min-[900px]:right-24 md:top-10 md:flex"
+          >
+            <span>Resume</span> <ArrowUpRight size={18} />
           </Link>
         </div>
-        <TabsContent value="home">
+        <TabsContent value="home" id="tabs-content-home">
           <Home />
         </TabsContent>
-        <TabsContent value="projects">
+        <TabsContent value="projects" id="tabs-content-projects">
           <AllProjects />
         </TabsContent>
       </Tabs>
